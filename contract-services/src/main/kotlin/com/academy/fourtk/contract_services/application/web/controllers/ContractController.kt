28f08b18d3ad3b484702d804d6aa54498d16a924
Dev-JeanSantos <jeancbsan@gmail.com>
2024-07-06 20:18:03 -0300

    ajustes endpoint contract retonar um 201

--- contract-services/src/main/kotlin/com/academy/fourtk/contract_services/application/web/controllers/ContractController.kt
+++ contract-services/src/main/kotlin/com/academy/fourtk/contract_services/application/web/controllers/ContractController.kt
@@ -5,6 +5,8 @@ import com.academy.fourtk.contract_services.application.web.controllers.builders
 import com.academy.fourtk.contract_services.application.web.controllers.dtos.requesties.ContractRequestV1
 import com.academy.fourtk.contract_services.application.web.controllers.dtos.responses.ContractResponseV1
 import com.academy.fourtk.contract_services.services.ServiceContract
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,9 +20,12 @@ class ContractController(
     @PostMapping
     fun createContract(
         @RequestBody contractRequestV1: ContractRequestV1,
-    ): ContractResponseV1 {
+    ): ResponseEntity<ContractResponseV1> {
         return runCatching {
-            service.create(buildContractEntity(contractRequestV1))
+            ResponseEntity(
+                service.create(buildContractEntity(contractRequestV1)),
+                HttpStatus.CREATED
+            )
         }.onFailure {
             buildResponse("123456", "falha")
         }.getOrThrow()
--- contract-services/src/main/kotlin/com/academy/fourtk/contract_services/resources/gateways/person/dto/PersonResponseData.kt
+++ contract-services/src/main/kotlin/com/academy/fourtk/contract_services/resources/gateways/person/dto/PersonResponseData.kt
@@ -1,6 +1,5 @@
 package com.academy.fourtk.contract_services.resources.gateways.person.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class PersonResponseData(
@@ -11,6 +10,5 @@ data class PersonResponseData(
     val cpf: String,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime? = null,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     val birthdayAt: String? = null
 )
