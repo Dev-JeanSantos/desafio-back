64a0d3d9aefc173c96225490f33413257bcae6e0
Dev-JeanSantos <jeancbsan@gmail.com>
2024-07-11 22:16:38 -0300

    implementacao Testes unitarios person e product gateway

---
+++ contract-services/src/test/kotlin/com/academy/fourtk/contract_services/resources/services/impl/PersonServiceImplTest.kt
@@ -0,0 +1,67 @@
+package com.academy.fourtk.contract_services.resources.services.impl
+
+import com.academy.fourtk.contract_services.application.web.config.parser.toObject
+import com.academy.fourtk.contract_services.domain.commons.PersonServiceIntegrationException
+import com.academy.fourtk.contract_services.resources.gateways.person.PersonGateway
+import com.academy.fourtk.contract_services.resources.gateways.person.dto.PersonResponseData
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@ExtendWith(SpringExtension::class)
+class PersonServiceImplTest {
+
+    @RelaxedMockK
+    private lateinit var gateway: PersonGateway
+
+    private lateinit var service: PersonServiceImpl
+
+    @BeforeEach
+    fun setup() {
+        service = PersonServiceImpl(
+            gateway
+        )
+    }
+
+    @Test
+    fun DeveraRetornarUmPersonComSucessoAoPassarUmaPersonIdValidoEExistenteRetornandoUmCodigoComStatus201() {
+
+        every { gateway.getPersonById(any()) } returns getPerson()
+
+        val person = service.getPersonById(getPerson().personId)
+
+        assertNotNull(person)
+        assertEquals(getPerson().personId, person.personId)
+        assertEquals(getPerson().cpf, person.cpf)
+        assertEquals(getPerson().birthdayAt, person.birthdayAt)
+        assertEquals(getPerson().gender, person.gender)
+        assertEquals(getPerson().firstName, person.firstName)
+        assertEquals(getPerson().lastName, person.lastName)
+
+        verify(exactly = 1) { gateway.getPersonById(any()) }
+    }
+
+    @Test
+    fun DeveraRetornarPersonServiceIntegrationExceptionQuandoAApiEstiverIndisponivel() {
+
+        every { gateway.getPersonById(any()) } throws PersonServiceIntegrationException("Integration Failure with PersonService")
+
+        assertThrows<PersonServiceIntegrationException> {
+            service.getPersonById(getPerson().personId)
+        }
+    }
+
+
+    private fun getPerson() = this::class.java.classLoader
+        .getResource("json/personResponseData.json")
+        ?.readBytes()!!.toString(Charsets.UTF_8).toObject<PersonResponseData>()
+}
+
+
---
+++ contract-services/src/test/kotlin/com/academy/fourtk/contract_services/resources/services/impl/ProductServiceImplTest.kt
@@ -0,0 +1,61 @@
+package com.academy.fourtk.contract_services.resources.services.impl
+
+import com.academy.fourtk.contract_services.application.web.config.parser.toObject
+import com.academy.fourtk.contract_services.domain.commons.ContractServiceIntegrationException
+import com.academy.fourtk.contract_services.resources.gateways.product.ProductGateway
+import com.academy.fourtk.contract_services.resources.gateways.product.dto.ProductResponseData
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class ProductServiceImplTest {
+
+    @RelaxedMockK
+    private lateinit var gateway: ProductGateway
+
+    private lateinit var service: ProductServiceImpl
+
+    @BeforeEach
+    fun setup() {
+        service = ProductServiceImpl(
+            gateway
+        )
+    }
+
+    @Test
+    fun DeveraRetornarUmProductComSucessoAoPassarUmaPersonIdValidoEExistenteRetornandoUmCodigoComStatus201() {
+
+        every { gateway.getProductById(any()) } returns getProduct()
+
+        val product = service.getProductById(getProduct().productId)
+
+        kotlin.test.assertNotNull(product)
+        kotlin.test.assertEquals(getProduct().productId, product.productId)
+        kotlin.test.assertEquals(getProduct().name, product.name)
+        kotlin.test.assertEquals(getProduct().origin, product.origin)
+        kotlin.test.assertEquals(getProduct().description, product.description)
+        kotlin.test.assertEquals(getProduct().quantity, product.quantity)
+
+        verify(exactly = 1) { gateway.getProductById(any()) }
+    }
+
+    @Test
+    fun DeveraRetornarProductServiceIntegrationExceptionQuandoAApiEstiverIndisponivel() {
+
+        every { gateway.getProductById(any()) } throws ContractServiceIntegrationException("Integration Failure with PersonService")
+
+        org.junit.jupiter.api.assertThrows<ContractServiceIntegrationException> {
+            service.getProductById(getProduct().productId)
+        }
+    }
+
+
+    private fun getProduct() = this::class.java.classLoader
+        .getResource("json/productResponseData.json")
+        ?.readBytes()!!.toString(Charsets.UTF_8).toObject<ProductResponseData>()
+}
